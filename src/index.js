bb3b713fdd6c0926ae99636ab60808a262b32f8d
SongLamPack <134986007+SongLamPack@users.noreply.github.com>
2023-12-12 09:09:37 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const URL =
-  "https://script.google.com/macros/s/AKfycbyrsvRx0R_bcCAULq0j_kvzTfGTNzWVBgVrc0ixUQY1oxSsOseVKiLx7rlUtSfzI6KY/exec";
+  "https://script.google.com/macros/s/AKfycbxR8Mav6na6elkJIEi28xvtfLCXFMJZ1Jmq1er009z7MYuhXGoHRI0bsOhp4W0b7FE-/exec";
 const dsvattu = document.querySelector(".dsvattu");
 const btnThemVT = document.getElementById("btnAdd");
 const btnguidk = document.getElementById("btnguidk");
